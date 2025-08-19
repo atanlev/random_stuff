aacfecb71e63fcf08eab1196e1ffb8c7b0a133e2
Ethan Lev <ethanlev123@gmail.com>
2025-08-19 14:26:46 +0300

    create bandpass for training

--- imu_filter.py
+++ imu_filter.py
@@ -8,12 +8,17 @@ from scipy import signal
 import numpy as np
 from torch_filter import torch_filtfilt, RealTimeIIR
 
-path = '/home/ethanl/data/mike-20250806_130228/happy_v3_1_22_07_25_7_/sim_data.pkl'
+# path = '/home/ethanl/data/mike-20250806_130228/happy_v3_1_22_07_25_7_/sim_data.pkl'
 # path = '/home/ethanl/data/neil-20250804_134921/develop_projects_happy_v3_1_29_07_25_8/sim_data.pkl'
 # path = '/home/ethanl/data/mike-20250806_142049/happy_v3_1_06_08_25_2_/sim_data.pkl'
 # path = '/home/ethanl/data/mike-20250805_153142/happy_v3_1_05_08_25_13_/sim_data.pkl'
+path = '/home/ethanl/data/logs/mike-20250818_153221/happy_v3_1_07_08_25_6_/sim_data.pkl' #unfiltered
+# path = '/home/ethanl/data/logs/mike-20250818_154125/happy_v3_1_18_08_25_1_/sim_data.pkl' #fir
 
-obs = 'get_imu_ang_v_local'
+# obs = 'get_imu_ang_v_local'
+# obs = 'get_imu_quat_normalized_heading'
+obs = 'get_imu_euler_normalized_heading'
+# obs = 'get_imu_ang_v_local_filtered_fir'
 
 def delay(b, a):
     w, h = freqz(b, a, worN=512)
@@ -25,6 +30,7 @@ def delay(b, a):
     w_gd = w[:-1]  # frequency grid for group delay
 
     return w, magnitude, phase, w_gd, group_delay
+
 def get_imu_data(data, sim_env, obs):
     obs_range = data['observation_idx_dict']['actor'][obs]
     idx = list(range(obs_range[0],obs_range[1]))
@@ -58,12 +64,12 @@ def FIR_filter(imu, ripple=20, width=300, fs=1000, cutoff=125):
 
     return filtered, taps, a
 
-def RT_FIR(imu, filter_rt):
+def RT_FIR(imu, filter_rt, state='real'):
     axes = ['x', 'y', 'z']
     rt_filtered = {'x': [],'y': [],'z': []}
 
     for axis in axes:
-        stream = imu['real'][axis] 
+        stream = imu[state][axis] 
         for sample in stream:
             y = filter_rt.step(sample)
             rt_filtered[axis].append(y.item())
@@ -101,7 +107,7 @@ def fft_plots(imu,filtered):
         imu_real_f = torch.fft.fft(imu['real'][axis])
         imu_sim_f = torch.fft.fft(imu['sim'][axis])
         print(f"Before filter distance in frequency domain {axes} axis:  {torch.norm(imu_sim_f-imu_real_f)}")
-        print(f"After filter distancein frequency domain {axes} axis: {torch.norm(filtered_f-imu_real_f)}")
+        print(f"After filter distancein frequency domain {axes} axis: {torch.norm(filtered_f-imu_sim_f)}")
 
         axs[idx].plot(xf, 2.0/N * torch.abs(imu_real_f[0:N//2]), label='Real')
         axs[idx].plot(xf, 2.0/N * torch.abs(imu_sim_f[0:N//2]), label='Sim')
@@ -137,19 +143,40 @@ def dist_plots(imu,filtered):
 
     plt.show()
 
+def filter_values(b, a):
+    w, mag, phase, w_gd, gd = delay(b, a)
+
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(3,1,1)
+    plt.plot(w, mag)
+    plt.title("Magnitude Response (dB)")
+
+    plt.subplot(3,1,2)
+    plt.plot(w, phase)
+    plt.title("Phase Response (rad)")
+
+    plt.subplot(3,1,3)
+    plt.plot(w_gd, gd)
+    plt.title("Group Delay (samples)")
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == '__main__':
-    sim_env = 1
+    sim_env = 0
     with open(path, 'rb') as f:
         data = pkl.load(f)
 
     imu = get_imu_data(data=data, sim_env=sim_env, obs=obs)
-    filtered_time, taps, a = FIR_filter(imu)
+    filtered, taps, a = FIR_filter(imu) #, ripple=10, width=300, fs=1000, cutoff=250)
     filter_rt = RealTimeIIR(taps, a)
-    filtered = RT_FIR(imu, filter_rt)
+    filtered_real = RT_FIR(imu, filter_rt)
+    filtered_sim = RT_FIR(imu, filter_rt)
 
-    magnitude, phase, group_delay = delay(taps, a)
-    time_domain_plots(imu, filtered)
-    fft_plots(imu, filtered)
-    dist_plots(imu, filtered)
+    time_domain_plots(imu, filtered_real)
+    fft_plots(imu, filtered_real)
+    dist_plots(imu, filtered_real)
+
+    filter_values(taps, a)
 
